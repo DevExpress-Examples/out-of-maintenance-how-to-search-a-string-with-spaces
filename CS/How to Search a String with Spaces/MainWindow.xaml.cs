127f248998f04c03ccc1b0f2569b588500341e58
Eugene <eugeniy.lapshov@devexpress.com>
2019-04-04 10:00:48 +0300

    Add files via upload

---
+++ CS/How to Search a String with Spaces/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using DevExpress.Data.Filtering;
+
+namespace How_to_Search_a_String_with_Spaces {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void TableView_SearchStringToFilterCriteria(object sender, DevExpress.Xpf.Grid.SearchStringToFilterCriteriaEventArgs e) {
+            e.Filter = CriteriaOperator.Parse(string.Format("Contains([Name], '{0}')", e.SearchString));
+        }
+    }
+}
---
+++ CS/How to Search a String with Spaces/ViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace How_to_Search_a_String_with_Spaces {
+    public class ViewModel {
+        ObservableCollection<Item> _items;
+        public IEnumerable<Item> Items { get { return _items; } }
+
+        public ViewModel() {
+            _items = new ObservableCollection<Item>();
+            for(int i = 0; i < 100; i++)
+                _items.Add(new Item(i));
+        }
+    }
+
+    public class Item : INotifyPropertyChanged {
+        static readonly Random rnd = new Random();
+        static readonly string[] Names = { "Alex", "Robert", "Anna", "Kate" };
+        static readonly string[] Surnames = { "Mercer", "Black", "White", "John" };    
+
+        public Item() { }
+
+        public Item(int id) : base() {
+            Id = id;
+            Name = string.Format("{0} {1}", Names[rnd.Next(Names.Length)], Surnames[rnd.Next(Surnames.Length)]);   
+        }
+
+        int _id;
+        public int Id { get { return _id; } set { _id = value; OnPropertyChanged("Id"); } }
+
+        string _name;
+        public string Name { get { return _name; } set { _name = value; OnPropertyChanged("Name"); } } 
+
+        void OnPropertyChanged(string propertyName) {
+            if(PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
